25bae39abf81e3d524676d18d522c02d05461495
Wyatt Childers <wchilders@nearce.com>
2019-01-04 20:09:54 -0500

    Updated build

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ad01f076be2f03c65a5d";
+/******/ 	var hotCurrentHash = "39cea1819cdc51be9c83";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ static/mumble-auto-connect.js
@@ -0,0 +1,5 @@
+(function() {
+  setTimeout(function() {
+    window.location = "mumble://voip.packetloss.gg:5360?title=Packet Loss Gaming&version=1.2.0";
+  }, 750);
+})();
