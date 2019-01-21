9f28e0e9a5b654844bb2a94c20030f27526e8f45
Wyatt Childers <wchilders@nearce.com>
2019-01-20 21:48:17 -0500

    Improved the connect system

--- pages/voip/connect/index.js
+++ pages/voip/connect/index.js
@@ -1,4 +1,6 @@
 import Bootstrap from '../../../components/Bootstrap'
+import NavBar from '../../../components/NavBar'
+import PrimaryContainer from '../../../components/PrimaryContainer'
 
 let VoIPConstants = require('../../../constants/Voip')
 
@@ -9,9 +11,40 @@ export default () => (
       <script charSet="UTF-8" src="/static/mumble-auto-connect.js"></script>
     </Bootstrap>
 
-    <div className="text-center mt-3">
-      If you're not automatically redirected, <a href="/voip/connect-info">click here</a>.
-    </div>
+    <PrimaryContainer>
+      <p className="lead text-center mb-5">
+        Your browser should prompt you momentarily to connect to Packet Loss Gaming Mumble server.<br />
+        If this does not occur, you may need to <a href="https://mumble.info" target="_blank">download Mumble</a>.
+      </p>
+
+      <p className="text-center">
+        <a href="/voip/connect">Retry Automatic Connection</a>
+        <span className="pl-3 pr-3 d-none d-sm-inline">&bull;</span>
+        <br className="mb-3 d-block d-sm-none"/>
+        <a data-toggle="collapse" href="#manualConnectionInfo" role="button" aria-expanded="false" aria-controls="collapseExample">
+          Show Manual Connection Information
+        </a>
+      </p>
+
+      <div className="collapse" id="manualConnectionInfo">
+        <table className="table table-borderless mx-auto" style={{maxWidth: '300px'}}>
+          <tbody>
+            <tr>
+              <th scope="row">Label</th>
+              <td>{ VoIPConstants.label }</td>
+            </tr>
+            <tr>
+              <th scope="row">Address</th>
+              <td>{ VoIPConstants.address }</td>
+            </tr>
+            <tr>
+              <th scope="row">Port</th>
+              <td>{ VoIPConstants.port }</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </PrimaryContainer>
   </div>
 )
 
--- static/mumble-auto-connect.js
+++ static/mumble-auto-connect.js
@@ -1,4 +1,14 @@
 (function() {
+  var LOCAL_STORAGE_KEY = "mumble-just-attempted-connection";
+
+  var getJustAttemptedConnection = function() {
+    return localStorage.getItem(LOCAL_STORAGE_KEY) === "true";
+  };
+
+  var setJustAttemptedConnection = function(value) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, value);
+  };
+
   var getBaseUrl = function() {
     var protocolInfo = location.protocol;
     var hostnameInfo = location.hostname;
@@ -9,9 +19,15 @@
 
   var originalBaseUrl = getBaseUrl();
 
+  if (getJustAttemptedConnection()) {
+    setJustAttemptedConnection(false);
+    return;
+  }
+
   setTimeout(function() {
-      window.location = originalBaseUrl + "/voip/connect-post";
-  }, 750);
+      setJustAttemptedConnection(true);
+      window.location = originalBaseUrl + "/voip/connect";
+  }, 1);
 
   window.location = "mumble://voip.packetloss.gg:5360?title=Packet Loss Gaming&version=1.2.0";
 })();
