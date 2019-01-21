9c7762e00048e8928ccdeb10a7119da8df626dbb
Wyatt Childers <wchilders@nearce.com>
2019-01-21 13:42:13 -0500

    Updated the navbar to conform to react specs

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,15 @@
 const NavBar = (props) => (
-  <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">Packet Loss Gaming</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+    <div className="container">
+      <a className="navbar-brand" href="/">Packet Loss Gaming</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/voip">VoIP</a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/voip">VoIP</a>
           </li>
         </ul>
       </div>
