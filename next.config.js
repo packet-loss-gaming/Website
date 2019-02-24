fc7f50aecb54949622bcd1c3e3593e34731b6ca7
Wyatt Childers <wchilders@nearce.com>
2019-02-23 21:41:37 -1000

    Ensure CNAME is copied with the build, removing a potential cause of site outage

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const fs = require('fs')
+const { join } = require('path')
+const { promisify } = require('util')
+const copyFile = promisify(fs.copyFile)
+
 module.exports = {
+  exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
+    if (dev) {
+      return defaultPathMap
+    }
+
+    await copyFile(join(dir, 'CNAME'), join(outDir, 'CNAME'))
+    return defaultPathMap
+  },
   webpack: (config) => {
     config.module.rules.push(
       {
