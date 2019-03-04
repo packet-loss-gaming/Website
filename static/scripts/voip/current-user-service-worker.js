2f1db02c6969741eabcf88a555e983cb0cdcf169
Wyatt Childers <wchilders@nearce.com>
2019-03-03 19:56:53 -0500

    Updated build

--- static/scripts/voip/current-user-service-worker.js
+++ static/scripts/voip/current-user-service-worker.js
@@ -6,8 +6,21 @@ let buildTitle = (data) => {
   }
 }
 
-self.addEventListener('push', event => {
+const ONE_SECOND = 1000
+const ONE_HOUR = 60 * 60 * ONE_SECOND
+const TWELVE_HOURS = 12 * ONE_HOUR
+
+let isExpiredNotification = (data) => {
+  let timeDiff = new Date() - Date.parse(data.sentAt)
+  return timeDiff > TWELVE_HOURS
+}
+
+self.addEventListener('push', (event) => {
   const data = event.data.json()
+
+  if (isExpiredNotification(data))
+    return
+
   self.registration.showNotification(buildTitle(data), {
     icon: 'https://packetloss.gg/static/favicon.png'
   })
