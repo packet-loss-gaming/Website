20e6de794055b59c9da4917142fb800f734c1dfd
Wyatt Childers <wchilders@nearce.com>
2019-03-05 17:26:50 -0500

    Use a fixed height to prevent jumping

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default () => (
     <NavBar></NavBar>
     <PrimaryContainer>
       <div className="d-none d-sm-block pt-5"></div>
-      <img className="rounded mx-auto d-block" style={{maxHeight: 10 + 'em'}} src="/static/favicon.png" />
+      <img className="rounded mx-auto d-block" style={{height: 10 + 'em'}} src="/static/favicon.png" />
       <div className="text-center">
         <h1>Packet Loss Gaming</h1>
         <p><i>Game with the finest</i></p>
