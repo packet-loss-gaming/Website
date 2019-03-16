ebc7afef0b31260e72e2cc0f1c84cf3b1d32599e
Wyatt Childers <wchilders@nearce.com>
2019-03-16 00:16:09 -0400

    Add cinematic splash video

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,38 @@ export default () => (
     <style jsx global>{`
       body {
         background: #515151;
+      }
+      #background {
+        filter: blur(15px);
+      }
+      #overlay {
         color: white;
         box-shadow: inset 0 0 15em black;
+        text-shadow: 2px 2px 8px #000000;
+      }
+      .splash-frame {
+        position: fixed;
+        left: 0px;
+        top: 0px;
         height: 100vh;
+        width: 100%;
       }
     `}</style>
-    <NavBar></NavBar>
-    <PrimaryContainer>
-      <div className="d-none d-sm-block pt-5"></div>
-      <img className="rounded mx-auto d-block" style={{height: 10 + 'em'}} src="/static/favicon.png" />
-      <div className="text-center">
-        <h1>Packet Loss Gaming</h1>
-        <p><i>Game with the finest</i></p>
-      </div>
-    </PrimaryContainer>
+    <div id="background" className="splash-frame">
+      <video preload="auto" autoplay="true" loop="loop" muted="muted" volume="0" style={{width: '100%', height: '100%', objectFit: 'cover'}}>
+        <source src="/static/videos/splash.webm" type="video/webm" />
+      </video>
+    </div>
+    <div id="overlay" className="splash-frame">
+      <NavBar></NavBar>
+      <PrimaryContainer>
+        <div className="d-none d-sm-block pt-5"></div>
+        <img className="rounded mx-auto d-block" style={{height: 10 + 'em'}} src="/static/favicon.png" />
+        <div className="text-center">
+          <h1>Packet Loss Gaming</h1>
+          <p><i>Game with the finest</i></p>
+        </div>
+      </PrimaryContainer>
+    </div>
   </div>
 )
