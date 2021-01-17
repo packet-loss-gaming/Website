891684282b95bee6f5a3621c3351e44cc9e5faef
Wyatt Childers <wchilders@nearce.com>
2021-01-17 17:25:13 -0500

    Remove voip section - unused

--- components/NavBarCore.js
+++ components/NavBarCore.js
@@ -12,17 +12,6 @@ const NavBarCore = (props) => (
         <li className="nav-item">
           <StyledLink className="nav-link" href="/coc">Code of Conduct</StyledLink>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            VoIP
-          </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <StyledLink className="dropdown-item" href="/voip">Overview</StyledLink>
-            <StyledLink className="dropdown-item" href="/voip/current-users">Current Users</StyledLink>
-            <div className="dropdown-divider"></div>
-            <StyledLink className="dropdown-item" href="/voip/connect">Quick Connect</StyledLink>
-          </div>
-        </li>
       </ul>
     </div>
   </div>
